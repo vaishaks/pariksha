fb86d1346fba3a3e39391c1ff3d2ff70fe739ba3
Vaishak Salin <vasal@microsoft.com>
2017-06-26 21:04:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ClickOutsideDirective } from './layout/clickOutsideDirective/click-outside.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  isSidePanelOpen = false;
+  hamburgerClicked = function hamburgerClicked() {
+    this.isSidePanelOpen = true;
+  };
+  outsideClicked = function outsideClicked() {
+    this.isSidePanelOpen = false;
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}
