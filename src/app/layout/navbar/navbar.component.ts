a71d3a84de5e35e185b0c0d42c79ef99545c2d37
Vaishak Salin <vasal@microsoft.com>
2017-07-02 19:33:27 +0530

    Added manifest

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -6,17 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  isHamburgerClicked = false;
   isSidePanelOpen = false;
   hamburgerClicked = function hamburgerClicked() {
+    this.isHamburgerClicked = true;
     this.isSidePanelOpen = true;
+  };
+  navbarClicked = function navabarClicked() {
+    if (this.isSidePanelOpen && this.isHamburgerClicked === false) {
+      this.isSidePanelOpen = false;
+    }
+    this.isHamburgerClicked = false;
   };
   outsideClicked = function outsideClicked() {
     this.isSidePanelOpen = false;
   };
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
