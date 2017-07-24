28e14d0379892fd6d0186b197bfc47c630b6860a
Vaishak Salin <vasal@microsoft.com>
2017-07-24 12:02:42 +0000

    Merged PR 381: Merge dev/vasal/deploy to develop

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
-ng build --prod
+ng build --prod --aot=false
 Set-Location -Path .\dist
 git init
+git config user.email "pariksha@microsoft.com"
 git add .
 git commit -m "initial"
 git remote add azure https://parikshadeploy:bunnycat43@parikshaweb-dev.scm.azurewebsites.net:443/parikshaweb-dev.git
