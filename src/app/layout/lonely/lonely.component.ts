b8a169de052d76948afb1596e01b6ba29a76633e
Vaishak Salin <vasal@microsoft.com>
2017-07-25 13:47:09 +0530

    Added save icon to the top navbar

---
+++ src/app/layout/lonely/lonely.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lonely',
+  templateUrl: './lonely.component.html',
+  styleUrls: ['./lonely.component.scss']
+})
+export class LonelyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
